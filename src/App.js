cbf1f595843ee469d81f3d3d7f124dde7c325be6
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-12-01 22:02:59 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import React, { useState, useEffect } from 'react'
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
+
+/* eslint-disable */
+
+function App() {
+  return <h2>slider project setup</h2>;
+}
+
+export default App;
